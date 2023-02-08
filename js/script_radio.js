a284978540939f5024ea0d310c53157c9724957e
Gabriel Castro <ghscastro@hotmail.com>
2023-02-07 21:56:02 -0300

    Início da seção POLO PASSIVO - 2ª Pessoa Jurídica.

--- js/script_radio.js
+++ js/script_radio.js
@@ -6,3 +6,12 @@ function habilitacao(){
       document.getElementById('input_nacionalidade_outra').disabled = false;
     }
   }
+
+  function habilitacao2(){
+    if(document.getElementById('input_nacionalidade_2a_pessoa_fisica_sim').checked == true){
+      document.getElementById('input_nacionalidade_2a_pessoa_fisica_outra').disabled = true;
+    }
+    if(document.getElementById('input_nacionalidade_2a_pessoa_fisica_sim').checked == false){
+      document.getElementById('input_nacionalidade_2a_pessoa_fisica_outra').disabled = false;
+    }
+  }
