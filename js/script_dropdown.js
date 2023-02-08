e1a5ebec62b12c9beee1f8fd3115c8b904727baf
Gabriel Castro <ghscastro@hotmail.com>
2023-02-08 10:09:29 -0300

    Finalização do segundo dropdown de estado civil.

--- js/script_dropdown.js
+++ js/script_dropdown.js
@@ -1,3 +1,4 @@
+/*Dropdown Primeiro Estado Civil*/
 function dropdown(p){
     var e = document.getElementsByClassName('dropdown')[0];
     var d = ['block','none'];
@@ -8,4 +9,19 @@ function dropdown(p){
 function category(c){
     var item = document.getElementById('item-'+c).innerHTML;
     document.getElementsByTagName('input')[8].value = item;
+}
+
+
+
+/*Dropdown Segundo Estado Civil*/
+function dropdown_2(t){
+    var b = document.getElementsByClassName('dropdown_2')[0];
+    var z = ['block','none'];
+
+    b.style.display = z[t];
+}
+
+function category_2(k){
+    var item_2 = document.getElementById('item_dp2-'+k).innerHTML;
+    document.getElementsByTagName('input')[32].value = item_2;
 }
