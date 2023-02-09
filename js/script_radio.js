2edf187bbb8a99d996c1f9fa50206aeb41ab70a0
Gabriel Castro <ghscastro@hotmail.com>
2023-02-09 13:37:40 -0300

    Finalização da subseção 1.  DADOS CONTRATUAIS. Adição do botão de submit. Teste do envio do formulário. TESTE: OK.

--- js/script_radio.js
+++ js/script_radio.js
@@ -15,3 +15,24 @@ function habilitacao(){
       document.getElementById('input_nacionalidade_2a_pessoa_fisica_outra').disabled = false;
     }
   }
+
+  function habilitacao3(){
+    if(document.getElementById('input_cumpriu_aviso_parcialmente_radio').checked == false){
+      document.getElementById('input_cumpriu_aviso_parcialmente').disabled = true;
+    }
+    if(document.getElementById('input_cumpriu_aviso_parcialmente_radio').checked == true){
+      document.getElementById('input_cumpriu_aviso_parcialmente').disabled = false;
+    }
+  }
+
+  function habilitacao4(){
+    if(document.getElementById('input_ctps_assinada_sim').checked == true){
+      document.getElementById('input_data_registro').disabled = false;
+    }
+    if(document.getElementById('input_ctps_assinada_sim').checked == false){
+      document.getElementById('input_data_registro').disabled = true;
+    }
+    if(document.getElementById('input_ctps_assinada_nao').checked == true){
+      document.getElementById('input_data_registro').disabled = true;
+    }
+  }
